cfcc87016e76e15405e72aced7ce29c6f770548a
Dirk Decher <dirk@decher.ch>
2023-04-28 15:53:57 +0200

    Vorbereitung für Prüfung Mai 2023

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
 import * as os from 'os';
 import * as pk from 'pkginfo';
 
@@ -16,32 +14,20 @@ const serverProtocol = process.env.SERVER_PROTOCOL || 'http';
 const httpInterface = process.env.SERVER_LISTEN_ON || '0.0.0.0';
 const accessServer = process.env.URI_SERVER || os.hostname();
 const port = process.env.SERVER_PORT || 3000;
-const apiName = process.env.API_NAME || 'api';
+// const apiName = process.env.API_NAME || 'api';
 
 // read from package.json
-const name = module.exports.name;
+// const name = module.exports.name;
 const version = module.exports.version;
-const description = module.exports.description;
-const authorInfo = module.exports.author.split('|');
-const licenseInfo = module.exports.license.split('|');
+// const description = module.exports.description;
+// const authorInfo = module.exports.author.split('|');
+// const licenseInfo = module.exports.license.split('|');
 
 async function bootstrap() {
   // enable CORS
   const app = await NestFactory.create(AppModule, { cors: true });
 
   // hint: add openApi
-  const config = new DocumentBuilder()
-    .setTitle(`REST ${name} app!`)
-    .setDescription(description)
-    .setVersion(version)
-    .setContact(authorInfo[0], authorInfo[1], authorInfo[2])
-    .setLicense(`Licence ${licenseInfo[0]}`, licenseInfo[1])
-    .setExternalDoc('Documentation', `/${apiName}-json`)
-    // add a bearer Authorize button
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
   // wir starten den Server
   await app.listen(port, httpInterface);
 

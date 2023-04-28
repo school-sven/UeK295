21f4190acac862f1bf16273a84c01af1215b222c
Dirk Decher <dirk@decher.ch>
2023-04-29 00:37:25 +0200

    security angepasst

--- src/sample/modules/auth/strategies/jwt.strategy.ts
+++ src/sample/modules/auth/strategies/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../auth.service/auth.service';
+import { Security } from '../../../../security';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -9,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET || 'No secret set...',
+      secretOrKey: Security.secret,
     });
   }
 

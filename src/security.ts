e67b6d3b8971db85381a986635b81be87f9be0c4
Sven <sven.maerki@gmx.ch>
2023-05-02 12:52:42 +0200

    Teilaufgabe 4 - Changed JWT Secret in .env and fallback secret in security.ts

--- src/security.ts
+++ src/security.ts
@@ -1,5 +1,16 @@
 export class Security {
+  /*
+  Hint: Diese Anpassung behebt ein potenzielles Sicherheitsproblem,
+  bei dem unbefugter Zugriff auf die Benutzerdaten möglich ist.
+  Durch diese Korrektur wird sichergestellt, dass nur authentifizierte
+  Benutzer auf die sensiblen Daten zugreifen können. Ebenfalls kann man sich
+  nicht mit einem von einer anderen Applikation generiertem Token anmelden (sofern
+  das JWT Secret in beiden Applikationen nicht gleich ist).
+   */
   static get secret() {
-    return process.env.JWT_SECRET || 'No secret set...';
+    return (
+      process.env.JWT_SECRET ||
+      'rhenweiun89fdsaASDFcibz=0-9u8y7t6r5e4w3q2!@#$%^&*()_+dsu87=0-9u8y7t6r5e4w3q2!@#$%^&*()_+dsu87'
+    );
   }
 }
